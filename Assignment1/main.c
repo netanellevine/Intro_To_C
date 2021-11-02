1d7397c30de4dc5085fc29c89efaff004b3e71c7
Netanel <netanellevine123@gmail.com>
2021-11-02 13:49:01 +0200

    update

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -7,28 +7,28 @@ int main() {
     scanf("%d", &start);
     printf("Enter the second number: \n");
     scanf("%d", &end);
-    printf("The Prime numbers between %d to %d are: ", start, end);
+    printf("The Prime numbers within the range of [%d,%d] are: ", start, end);
     for(i = start; i <= end; i++ ){
         if(isPrime(i) == TRUE){
             printf("%d ", i);
         }
     }
     printf("\n");
-    printf("The Armstrong numbers between %d to %d are: ", start, end);
+    printf("The Armstrong numbers within the range of [%d,%d] are: ", start, end);
     for(i = start; i <= end; i++ ){
         if(isArmstrong(i) == TRUE){
             printf("%d ", i);
         }
     }
     printf("\n");
-    printf("The Strong numbers between %d to %d are: ", start, end);
+    printf("The Strong numbers within the range of [%d,%d] are: ", start, end);
     for(i = start; i <= end; i++ ){
         if(isStrong(i) == TRUE){
             printf("%d ", i);
         }
     }
     printf("\n");
-    printf("The Palindrome numbers between %d to %d are: ", start, end);
+    printf("The Palindrome numbers within the range of [%d,%d] are: ", start, end);
     for(i = start; i <= end; i++ ){
         if(isPalindrome(i) == TRUE){
             printf("%d ", i);
